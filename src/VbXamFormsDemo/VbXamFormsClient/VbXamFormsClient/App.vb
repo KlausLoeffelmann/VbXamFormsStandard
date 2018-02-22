60fcfddb6d7e2df9f63541224134e30e61a2a3ab
Klaus Löffelmann <klaus@loeffelmann.de>
2018-02-22 10:58:37 +0100

    Convert the VbXamarinDemo from the Vb Team Blog to be based on the Xamarin.Forms .NET Standard template.

---
+++ src/VbXamFormsDemo/VbXamFormsClient/VbXamFormsClient/App.vb
@@ -0,0 +1,22 @@
+﻿Imports Xamarin.Forms
+
+Public Class App
+    Inherits Application
+
+    Sub New()
+        MainPage = New MainPage
+    End Sub
+
+    Protected Overrides Sub OnStart()
+        ' Handle when your app starts
+    End Sub
+
+    Protected Overrides Sub OnSleep()
+        ' Handle when your app sleeps
+    End Sub
+
+    Protected Overrides Sub OnResume()
+        ' Handle when your app resumes
+    End Sub
+
+End Class
---
+++ src/VbXamFormsDemo/VbXamFormsClient/VbXamFormsClient/MainPage.vb
@@ -0,0 +1,64 @@
+Imports Xamarin.Forms
+
+Public Class MainPage
+    Inherits ContentPage
+
+    Dim myDecimalEntry As New Entry With
+        {.Placeholder = "Enter decimal number",
+         .Margin = New Thickness(5, 5, 5, 5),
+         .VerticalOptions = LayoutOptions.CenterAndExpand,
+         .FontSize = 24}
+
+    Dim WithEvents myConvertButton As New Button With
+        {.Text = "Convert!",
+         .VerticalOptions = LayoutOptions.CenterAndExpand,
+         .HorizontalOptions = LayoutOptions.CenterAndExpand,
+         .FontSize = 24}
+
+    Dim myResultLabel As New Label With
+        {.Text = "- - -",
+         .FontSize = 36,
+         .VerticalOptions = LayoutOptions.CenterAndExpand,
+         .HorizontalOptions = LayoutOptions.CenterAndExpand}
+
+    Private Sub ConvertButton_Clicked(sender As Object,
+                                        e As EventArgs) Handles myConvertButton.Clicked
+
+        myResultLabel.Text = RomanNumerals.
+                    RomanNumeralFromValue(Integer.Parse(myDecimalEntry.Text))
+
+    End Sub
+
+    Public Sub New()
+
+        Padding = New Thickness(0, 10, 0, 10)
+        Me.BackgroundColor = Color.CornflowerBlue
+
+        Dim grid = New Grid With {
+            .HorizontalOptions = LayoutOptions.FillAndExpand,
+            .VerticalOptions = LayoutOptions.FillAndExpand,
+            .RowDefinitions = New RowDefinitionCollection From
+            {
+                New RowDefinition With {.Height = 80},
+                New RowDefinition With {.Height = GridLength.Star},
+                New RowDefinition With {.Height = GridLength.Star},
+                New RowDefinition With {.Height = New GridLength(2, GridUnitType.Star)}
+            }
+        }
+
+        grid.Children.Add(New Label With {
+                            .Text = "Roman Numerals",
+                            .VerticalOptions = LayoutOptions.CenterAndExpand,
+                            .HorizontalOptions = LayoutOptions.CenterAndExpand,
+                            .FontSize = 36,
+                            .FontAttributes = FontAttributes.Bold
+                          }, 0, 0)
+
+        grid.Children.Add(myDecimalEntry, 0, 1)
+        grid.Children.Add(myConvertButton, 0, 2)
+        grid.Children.Add(myResultLabel, 0, 3)
+        Content = grid
+
+    End Sub
+
+End Class
